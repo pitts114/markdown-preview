eee6afa79b5926b2050ec129bfb8c8e220d0281d
pullman14 <pitts.114@buckeyemail.osu.edu>
2017-08-27 19:32:59 -0400

    Add TextBox

--- src/index.js
+++ src/index.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+var marked = require("marked")
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      markedStr: ''
+    }
+  }
+
+  update(str) {
+    this.setState({markedStr: str})
+  }
+
   render() {
-    return(
-      <h1>Hello World!</h1>
+    return (
+      <div class="container">
+        <div class="row">
+          <TextBox/>
+        </div>
+      </div>
     )
   }
 }
 
+class TextBox extends React.Component {
+  render() {
+    return (
+      <div class="col-xs-12 col-sm-6">
+        <div class="form-group">
+          <label for="code">Type Markdown here:</label>
+          <br></br>
+          <textarea class="form-control" rows="10" cols="40" id="code" onKeyPress={this.props.update}></textarea>
+        </div>
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function MarkdownBox() {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+ReactDOM.render(
+  <App/>, document.getElementById('root'));
